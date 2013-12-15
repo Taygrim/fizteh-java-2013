c390118b2e51d04162082569a4682b9343d422fb
Taygrim <tagrimar@mail.ru>
2013-12-15 21:26:15 +0400

    commit

--- src/ru/fizteh/fivt/students/drozdowsky/database/MfhmProviderFactory.java
+++ src/ru/fizteh/fivt/students/drozdowsky/database/MfhmProviderFactory.java
@@ -7,15 +7,10 @@ import java.io.File;
 import java.io.IOException;
 
 public class MfhmProviderFactory implements TableProviderFactory {
-    public MultiFileHashMap create(String dir) {
+    public MultiFileHashMap create(String dir) throws IOException {
         if (!Utils.isValid(dir) || !(new File(dir).isDirectory())) {
             throw new IllegalArgumentException();
         }
-        try {
-            return new MultiFileHashMap(dir);
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-            return null;
-        }
+        return new MultiFileHashMap(dir);
     }
 }
