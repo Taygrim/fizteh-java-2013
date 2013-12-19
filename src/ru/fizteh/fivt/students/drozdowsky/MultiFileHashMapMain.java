f3cce5c6b793d368457fbbd9e5e5d6eae251a95c
Taygrim <tagrimar@mail.ru>
2013-12-19 17:29:27 +0300

    commit

--- src/ru/fizteh/fivt/students/drozdowsky/MultiFileHashMapMain.java
+++ src/ru/fizteh/fivt/students/drozdowsky/MultiFileHashMapMain.java
@@ -3,7 +3,6 @@ package ru.fizteh.fivt.students.drozdowsky;
 import ru.fizteh.fivt.storage.structured.Storeable;
 import ru.fizteh.fivt.students.drozdowsky.commands.MfhmController;
 import ru.fizteh.fivt.students.drozdowsky.database.FileHashMap;
-import ru.fizteh.fivt.students.drozdowsky.utils.Storable;
 import ru.fizteh.fivt.students.drozdowsky.utils.Utils;
 import ru.fizteh.fivt.students.drozdowsky.modes.ModeController;
 import ru.fizteh.fivt.students.drozdowsky.database.MultiFileHashMap;
@@ -17,6 +16,19 @@ import java.util.HashMap;
 public class MultiFileHashMapMain {
 
     public static void main(String[] args) {
+        /*try {
+            MultiFileHashMap temp = new MultiFileHashMap("/Users/00/Documents/Programming/Java/multibd");
+            FileHashMap temp2 = temp.getTable("asd");
+            //Storeable temp3 = temp.createFor(temp2);
+            //temp2.put("asd", temp3);
+            //temp2.commit();
+            Storeable value = temp.createFor(temp2);
+            value.setColumnAt(0, (byte) 123);
+            value.setColumnAt(1, false);
+            temp2.put("asd", value);
+        } catch (IOException e) {
+
+        }   */
         String dbDirectory = System.getProperty("fizteh.db.dir");
         if (dbDirectory == null) {
             System.err.println("No database location");
